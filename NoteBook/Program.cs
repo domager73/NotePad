3cb7f82d6d13f3d157146994a25f480d2b07acde
domager73 <112970685+domager73@users.noreply.github.com>
2023-04-02 11:27:53 +0300

    .

--- NoteBook/Program.cs
+++ NoteBook/Program.cs
@@ -17,6 +17,7 @@ namespace NoteBook
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMain());
+
         }
     }
 }
